08b0a4ec7e70551e32602ea4d7a9b456caabd5cc
rohit <rohit@LAPTOP-ORNCMIEK>
2021-02-25 11:22:57 +0530

    firstproject

---
+++ Testproject/src/main/java/som/cts/maven/Testproject/App.java
@@ -0,0 +1,13 @@
+package som.cts.maven.Testproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
